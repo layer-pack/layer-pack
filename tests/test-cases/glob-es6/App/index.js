679e9193b5d93ca58fb6ee5a0313a92eff14017f
nathb <nathb2b@gmail.com>
2019-07-25 13:34:55 +0200

    ^

--- tests/test-cases/glob-es6/App/index.js
+++
@@ -1,3 +0,0 @@
-export {default as allCodeTree}  from "App/pets/(**/*).js"
-export {default as allFoodMngrs} from "App/pets/(*)/foodManager.js"
-export {default as allGameMngrs} from "App/pets/(*)/gameManager.js"
