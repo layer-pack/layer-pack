6f8f3af29293d1bde864b79b6134c020a6e828bf
nathb <nathb2b@gmail.com>
2020-12-30 11:37:09 +0100

    ^licenses

--- etc/cli/utils.js
+++ etc/cli/utils.js
@@ -1,9 +1,6 @@
 /*
  *   The MIT License (MIT)
- *   Copyright (c) 2019. Wise Wild Web
- *   MIT License
- *
- *   Copyright (c) 2018 Wise Wild Web
+ *   Copyright (c) 2020. Nathanael Braun
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
